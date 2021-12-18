04a6ac87bd9d70932a8c9e5f822083f81cb6bea8
Ivan-Mol <abyss.devotee@gmail.com>
2021-12-18 14:15:19 +0300

    Add Lesson 2

--- src/main/java/com/lessons/lesson2partThreeEnum/MainApp.java
+++
@@ -1,7 +0,0 @@
-package com.lessons.lesson2partThreeEnum;
-
-public class MainApp {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/main/java/com/lessons/lesson2partThreeEnumAndInnerClass/MainApp.java
@@ -0,0 +1,25 @@
+package com.lessons.lesson2partThreeEnumAndInnerClass;
+//внешний класс - не имеет доступа к внутреннему
+public class MainApp {
+    int outherInt = 1;
+
+    public MainApp(int outherUnt) {
+        this.outherInt = outherUnt;
+    }
+//вложенный класс - имеет доступ к внешнему
+    public class InnerClass{
+        int innerInt = 0;
+
+        public InnerClass(int innerInt) {
+            this.innerInt = innerInt;
+        }
+
+        public void innerClassMethod(){
+            System.out.println(outherInt);
+        }
+    }
+    public static void main(String[] args) {
+
+
+    }
+}
