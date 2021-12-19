07f72cc06280a48a3fd316773b89565f8111b9fc
Ivan-Mol <abyss.devotee@gmail.com>
2021-12-19 18:20:28 +0300

    Lesson 2

--- src/main/java/com/lessons/lesson2partThreeEnumAndInnerClass/MainApp.java
+++ src/main/java/com/lessons/lesson2partThreeEnumAndInnerClass/MainApp.java
@@ -1,12 +1,17 @@
 package com.lessons.lesson2partThreeEnumAndInnerClass;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 //внешний класс - не имеет доступа к внутреннему
 public class MainApp {
-    int outherInt = 1;
+    int outerInt = 1;
 
-    public MainApp(int outherUnt) {
-        this.outherInt = outherUnt;
+    public MainApp(int outerUnt) {
+        this.outerInt = outerUnt;
     }
-//вложенный класс - имеет доступ к внешнему
+//внутренний класс - имеет доступ к внешнему
     public class InnerClass{
         int innerInt = 0;
 
@@ -15,12 +20,37 @@ public class MainApp {
         }
 
         public void innerClassMethod(){
-            System.out.println(outherInt);
+            System.out.println(outerInt);
         }
+    //Так получается потому что внутренний не может существовать без внешнего, а внешний без внутреннего может.
     }
-//Так получается потому что внутренний не может существовать без внешнего, а внешний без внутреннего может.
+
+    //static делает внутренний класс вложенным - можно создавать объекты из внешнего - но теряется доступ к объектам внешнего класса
+    public static class InnerClass2{
+
+    }
+
     public static void main(String[] args) {
+      //  InnerClass innerClass1 = new InnerClass(); - не сработает
+        InnerClass2 innerClass2 = new InnerClass2(); // сработает
+
+        //локальный класс - класс внутри метода
+        class Point {
+            int x = 1;
+
+            public Point(int x) {
+                this.x = x;
+            }
 
 
+        }
+        //кнопка с анонимным классом в методе "при нажатии кнопки"
+        JButton button = new JButton();
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("123");
+            }
+        });
     }
 }
