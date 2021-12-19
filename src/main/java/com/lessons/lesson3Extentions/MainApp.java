d7dbe73dec1ad83ae7994e3474bc4a8595f1a368
Ivan-Mol <abyss.devotee@gmail.com>
2021-12-19 17:23:31 +0300

    Lesson 2

--- src/main/java/com/lessons/lesson3Extentions/MainApp.java
+++ src/main/java/com/lessons/lesson3Extentions/MainApp.java
@@ -1,4 +1,28 @@
 package com.lessons.lesson3Extentions;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class MainApp {
+    //исключения
+    public static void main(String[] args) {
+
+        //обрабатываем ошибку которая не была обработана в методе
+        try {
+            doSomething();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    // throws чтобы не обрабатывать исключения в методе, должен обрабатывать тот кто вызвал метод выше по стеку
+    public static void doSomething() throws FileNotFoundException {
+        FileOutputStream fileOutputStream = new FileOutputStream("fileName");
+    }
+
+
+
+
 }
