6f5ae511a8646cc07868dead71def148262b1450
Ivan-Mol <abyss.devotee@gmail.com>
2021-12-24 18:18:29 +0300

    Lesson 4

--- src/main/java/com/lessons/lesson4PartOneGenerics/MainApp.java
+++ src/main/java/com/lessons/lesson4PartOneGenerics/MainApp.java
@@ -1,7 +1,21 @@
 package com.lessons.lesson4PartOneGenerics;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+//Generics Обобщения
 public class MainApp {
     public static void main(String[] args) {
+        genBox<String> strBox = new genBox<>("String");
+        strBox.setObj("String2"); //Уже не поставить ничего кроме строки
+        genBox<Integer> intBox1 = new genBox<>(21); //примитивный тип заворачивается в Integer
+        genBox<Integer> intBox2 = new genBox<>(12);
+        System.out.println(intBox1.getObj()+intBox2.getObj());
+
+        TestBox<Number> testBox1 = new TestBox<>(12,32,12,412);
+        System.out.println(testBox1.average());
+        TestBox<Number> testBox2 = new TestBox<>(14,2,12,42,15,2,21,23,51,72);
 
     }
+
 }
