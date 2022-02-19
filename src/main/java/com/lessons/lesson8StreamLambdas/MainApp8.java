c87340e081042a6720ad43e4fcf31a6e56660c33
Ivan-Mol <abyss.devotee@gmail.com>
2022-02-19 17:46:41 +0300

    Lesson 4

--- src/main/java/com/lessons/lesson8StreamLambdas/MainApp8.java
+++ src/main/java/com/lessons/lesson8StreamLambdas/MainApp8.java
@@ -1,4 +1,24 @@
 package com.lessons.lesson8StreamLambdas;
 
 public class MainApp8 {
+    public static void main(String[] args) {
+        //в методе main переопределям run в анонимном классе
+        doSomething(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Первый вариант");
+            }
+        });
+        doSomething(()-> System.out.println("Второй вариант с лямбдой"));
+
+        doSomething(()->{
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Третий вариант с лямбдой");
+            }
+        });
+    }
+    //создаем метод в который можно пробросить нужный нам метод run в объекте
+    public static void doSomething(Runnable runnableObj){
+        runnableObj.run();
+    }
 }
--- src/main/java/com/lessons/testWork.java
+++ src/main/java/com/lessons/testWork.java
@@ -4,6 +4,17 @@ import java.util.Random;
 
 public class testWork {
     public static void main(String[] args) {
+        inter inter = ()-> System.out.println("asd");
+        inter inter2 = ()-> System.out.println("aasd");
+        inter.printSmt();
+        inter2.printSmt();
 
+
+    }
+
+
+    @FunctionalInterface
+    public interface inter{
+        public void printSmt();
     }
 }
